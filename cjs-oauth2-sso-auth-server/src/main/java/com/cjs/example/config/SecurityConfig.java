820fdf96f65fd220bd09d7e9de16d01d3cc1242c
Chaly <i@eunut.com>
2019-01-24 15:30:04 +0800

    关闭csrf保证logout的get请求可以访问

--- cjs-oauth2-sso-auth-server/src/main/java/com/cjs/example/config/SecurityConfig.java
+++ cjs-oauth2-sso-auth-server/src/main/java/com/cjs/example/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/oauth/**","/login/**", "/logout").permitAll()
                 .anyRequest().authenticated()   // 其他地址的访问均需验证权限
                 .and()
